b931a39317d119c903aec66e8d661b55bc73d69e
niao003 <>
2026-09-18 14:48:23 +0800

    chore: remove accidental local artifacts

--- openceo_patch/repo/app/Domain/OpenCEO/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Leantime\Domain\OpenCEO\Controllers\Desk;
-
-Route::get('/openceo/desk', [Desk::class, 'show'])->name('openceo.desk');
-Route::post('/openceo/desk', [Desk::class, 'post']);
